119f1ad00e9239684d8cf964a6f66b963f1794d5
ma2calllove <ma2calllove@host.docker.internal>
2025-01-20 17:22:20 +0900

    test

---
+++ 01.shok-test01/src/Hello.java
@@ -0,0 +1,15 @@
+/**
+ * @author ma2calllove
+ *
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
