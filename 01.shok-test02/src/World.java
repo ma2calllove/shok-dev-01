cf1fed93dae729f84323206062091f69d7d86b7a
ma2calllove <ma2calllove@host.docker.internal>
2025-01-20 17:48:22 +0900

    0

---
+++ 01.shok-test02/src/World.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author ma2calllove
+ *
+ */
+public class World {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
